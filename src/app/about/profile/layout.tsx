7075278b5261c8c20ecc079691682c9c2f73a54e
brianajiks123 <brianajiks123@gmail.com>
2025-05-29 19:48:23 +0700

    Add Layout and Template

---
+++ src/app/about/profile/layout.tsx
@@ -0,0 +1,8 @@
+export default function ProfileLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <>
+            <h1>Hello</h1>
+            <div>{children}</div>
+        </>
+    )
+}
